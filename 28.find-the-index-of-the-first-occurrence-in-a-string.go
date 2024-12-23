9b7d674a14ab66c71ef782bb30e534546750682a
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2024-12-23 16:52:34 +0800

    Finished no.28

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=28 lang=golang
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+
+	if len(needle) == 0 {
+		return 0
+	}
+
+	m, n := len(needle), len(haystack)
+
+	// Sliding window approach
+	//n-m 為了限制不要超出比較的長度不要超出needle的長度
+	//haystack = "leetcode", needle = "leeto"
+	// i ~ i+m 每移動一次,就跟著i+m
+
+	for i := 0; i <= n-m; i++ {
+		if haystack[i:i+m] == needle {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// @lc code=end
+
