e9277cb6600ba93842d7ee6f3a51cee20b6abd57
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2024-12-23 18:07:49 +0800

    Finished No.58

---
+++ 58.length-of-last-word.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=58 lang=golang
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+func lengthOfLastWord(s string) int {
+	// Trim trailing spaces to handle cases like "Hello World   "
+	s = strings.TrimSpace(s)
+
+	// Split the string by spaces
+	words := strings.Split(s, " ")
+
+	// Return the length of the last word
+	return len(words[len(words)-1])
+}
+
+// @lc code=end
+
