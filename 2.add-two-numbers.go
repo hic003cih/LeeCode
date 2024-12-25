ff417e7667e8483200f2289ada9d8d377a4664f3
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2024-12-25 15:37:17 +0800

    Finished No.2

--- 2.add-two-numbers.go
+++ 2.add-two-numbers.go
@@ -1,48 +1,40 @@
-package leetcode
-
 /*
  * @lc app=leetcode id=2 lang=golang
  *
  * [2] Add Two Numbers
  */
 
-// Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	// Initialize a dummy node to simplify the code
 	dummy := &ListNode{}
 	current := dummy
 	carry := 0
 
-	// Traverse both lists
-	for l1 != nil || l2 != nil {
-		// Get the values from the current nodes, if they exist
-		var x, y int
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
 		if l1 != nil {
-			x = l1.Val
+			sum += l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
-			y = l2.Val
+			sum += l2.Val
 			l2 = l2.Next
 		}
 
-		// Calculate the sum and the carry
-		sum := x + y + carry
 		carry = sum / 10
 		current.Next = &ListNode{Val: sum % 10}
 		current = current.Next
 	}
 
-	// If there's a remaining carry, add a new node
-	if carry > 0 {
-		current.Next = &ListNode{Val: carry}
-	}
-
-	// Return the next node of dummy, which is the head of the new list
 	return dummy.Next
 }
+
+// @lc code=end
+
