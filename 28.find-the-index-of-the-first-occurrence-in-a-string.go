10aa1f50e93bb1f1d4b3eb4d0e1bdd8b16e6e8d5
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2025-01-02 17:13:18 +0800

    Reviwed No28

--- 28.find-the-index-of-the-first-occurrence-in-a-string.go
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.go
@@ -4,6 +4,9 @@
  * [28] Find the Index of the First Occurrence in a String
  */
 
+//haystack = "sadbutsad", needle = "sad"
+// Output:0
+
 // @lc code=start
 func strStr(haystack string, needle string) int {
 
@@ -11,20 +14,36 @@ func strStr(haystack string, needle string) int {
 		return 0
 	}
 
+	//3, 9
 	m, n := len(needle), len(haystack)
 
-	// Sliding window approach
-	//n-m 為了限制不要超出比較的長度不要超出needle的長度
-	//haystack = "leetcode", needle = "leeto"
-	// i ~ i+m 每移動一次,就跟著i+m
+	// i < 6
 
-	for i := 0; i <= n-m; i++ {
+	for i := 0; i < n-m; i++ {
 		if haystack[i:i+m] == needle {
 			return i
 		}
 	}
-
 	return -1
+
+	// if len(needle) == 0 {
+	// 	return 0
+	// }
+
+	// m, n := len(needle), len(haystack)
+
+	// // Sliding window approach
+	// //n-m 為了限制不要超出比較的長度,不要超出needle的長度
+	// //haystack = "leetcode", needle = "leeto"
+	// // i ~ i+m 每移動一次,就跟著i+m
+
+	// for i := 0; i <= n-m; i++ {
+	// 	if haystack[i:i+m] == needle {
+	// 		return i
+	// 	}
+	// }
+
+	// return -1
 }
 
 // @lc code=end
