1243136e3ae9efa94f5cb804f242335ebe5f474a
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2025-06-24 15:25:13 +0800

    Reviewed No.349

--- 349.intersection-of-two-arrays.py
+++ 349.intersection-of-two-arrays.py
@@ -15,49 +15,51 @@ class Solution:
         # for num1 in nums1:
         #     # Inner Loop: Compares the current element from nums1 with each element in the second list (nums2)
         #     for num2 in nums2:
-        #         # If a matching element is found (num1==num2)
-        #         # And this element is not yet in our result list
+        #         # If a match is found (num1==num2)
+        #         # And this element is not already in our result list
         #         # Then add it to the result list
         #         if num1 == num2 and num1 not in result:
         #             result.append(num1)
-
+        #             # Once a match is found and added, break the inner loop to move to the next element in nums1
+        #             break
         # return result
 
-        # # Sorting + Two Pointers
+        #    # Sorting + Two Pointers
 
-        # if not nums1 or not nums2:
-        #     return []
-        # # Sort both input lists to enable the tow-pointer approach
-        # nums1.sort()
-        # nums2.sort()
+        #     if not nums1 or not nums2:
+        #         return []
+        #     # Sort both input lists to enable the tow-pointer approach
+        #     nums1.sort()
+        #     nums2.sort()
 
-        # n1 = len(nums1)
-        # n2 = len(nums2)
+        #     n1 = len(nums1)
+        #     n2 = len(nums2)
 
-        # # Initialize two pointers, one for each list, to traverse them.
-        # ptr1 = 0
-        # ptr2 = 0
-        # result = []
+        #     # Initialize two pointers, one for each list, to traverse them.
+        #     ptr1 = 0
+        #     ptr2 = 0
+        #     result = []
 
-        # # Iterate through the number list
-        # while ptr1 < n1 and ptr2 < n2:
-        #     # If the element in nums1 is smaller than the element in nums2
-        #     # This means nums1[ptr1] cannot be in the intersection with the current nums[ptr2].
-        #     if nums1[ptr1] < nums2[ptr2]:
-        #         ptr1+=1
-        #     # If the element in nums1 is bigger than the current number in nums2
-        #     # This means nums1[ptr1] cannot be in the intersection with the current nums[ptr2].
-        #     elif nums1[ptr1] > nums2[ptr2]:
-        #         ptr2 +=1
-        #     else:
-        #         # If the elements are equal, a common element is found.
-        #         # Add the number to the result if it's not already he last added element (to handle duplicates)
-        #         if not result or result[-1] != nums1[ptr1]:
-        #             result.append(nums1[ptr1])
-                
-        #         ptr1 +=1
-        #         ptr2 +=1
-        # return result
+        #     # Iterate through the number list
+        #     while ptr1 < n1 and ptr2 < n2:
+        #         # If the element in nums1 is smaller than the element in nums2
+        #         # This means nums1[ptr1] cannot be in the intersection with the current nums[ptr2].
+        #         if nums1[ptr1] < nums2[ptr2]:
+        #             ptr1+=1
+        #         # If the element in nums1 is bigger than the current number in nums2
+        #         # This means nums1[ptr1] cannot be in the intersection with the current nums[ptr2].
+        #         elif nums1[ptr1] > nums2[ptr2]:
+        #             ptr2 +=1
+        #         else:
+        #             # We need 'not result' to handle the case when 'result' is empty to avoid an IndexError when accessing result[-1]
+        #             # If the elements are equal, a common element is found.
+        #             # Add the number to the result if it's not already he last added element (to handle duplicates)
+        #             if not result or result[-1] != nums1[ptr1]:
+        #                 result.append(nums1[ptr1])
+                    
+        #             ptr1 +=1
+        #             ptr2 +=1
+        #     return result
         
         # # Hash Map(Frequency Counting) Approach
         # if not nums1 or not nums2:
@@ -66,7 +68,7 @@ class Solution:
         # # First pass : Populate the frequency map(counts) with elements from nums1.
         # for num in nums1:
         #     counts[num]+=1
-        
+        # # We use a set for the result because the problem asks for unique intersection elements.
         # result_set = set()
 
         # for num in nums2:
@@ -74,20 +76,19 @@ class Solution:
         #     if num in counts and counts[num] > 0:
         #         # Add it to our result set
         #         result_set.add(num)
+        # # Finally, convert the result set to a list
         # return list(result_set)
-
-        #Optimal approach - Using set Intersection
-        # Convert both lists to sets, sets inherently handle uniqueness of elements
-        set1 = set(nums1)
-        set2 = set(nums2)
-
-        # Perform the intersection operation using the '&' operator (or .intersection() method).
-        # The result is a new set containing only elements present in both original sets.
-        # it can also become: intersection_set = set1.intersection(set2)
-        intersection_set = set1 & set2
-
-        return list(intersection_set) 
         
+        # #Optimal approach - Using set Intersection
+        # # Convert both lists to sets, sets inherently handle uniqueness of elements
+        # set1 = set(nums1)
+        # set2 = set(nums2)
+
+        # # Perform the intersection operation using the '&' operator (or .intersection() method).
+        # # The result is a new set containing only elements present in both original sets.
+        # # it can also become: intersection_set = set1.intersection(set2)
+        # intersection_set = set1 & set2
+
+        # return list(intersection_set) 
     
 # @lc code=end
-
