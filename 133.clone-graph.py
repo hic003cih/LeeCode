a3a6a5cdf22f78a3332f424d082cc6e42351539c
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2025-07-24 15:24:44 +0800

    Completed in No.133

--- 133.clone-graph.py
+++ 133.clone-graph.py
@@ -1,5 +1,5 @@
 #
-# @lc app=leetcode id=133 lang=python
+# @lc app=leetcode id=133 lang=python3
 #
 # [133] Clone Graph
 #
@@ -7,53 +7,65 @@
 # @lc code=start
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-    # DFS(Depth-First Search)
+from typing import Optional
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # # 1. DFS + HashMap
+        # # A map to store the mapping from original nodes to their clones.
+        # # This prevents infinite loops in case of cycles.
+        # cloned_nodes ={}
+        # def dfs_clone(original_node):
+        #     #If the node is already cloned, return the clone from the map.
+        #     if original_node in cloned_nodes:
+        #         return cloned_nodes[original_node]
+        #     #Create a new node with the same value.
+        #     copy_node = Node(original_node.val)
 
-        # 如果節點不存在，則返回None
-        # If the node does not exist, return None
+        #     #Add the new node to the map Before traversing its neighbors.
+        #     #This handles cycles.
+        #     cloned_nodes[original_node] = copy_node
+
+        #     #Recursively clone all the neighbors.
+        #     if original_node.neighbors:
+        #         for neighbor in original_node.neighbors:
+        #             copy_node.neighbors.append(dfs_clone(neighbor))
+        #     return copy_node
+        # # The entry point of the cloning process.
+        # # Handle the case where the input graph is empty.
+        # return dfs_clone(node) if node else None
+
+        # 2. BFS + HashMap
         if not node:
             return None
-        
-        # 創建一個字典，用於存儲已訪問過的節點
-        # Create a dictionary to store the visited nodes
-        visited = {}
+        # Map to store the mapping from original nodes to their clones.
+        cloned_nodes = {node: Node(node.val)}
 
-        def dfs(node):
+        # Queue for BFS, storing original nodes that need to be processed.
+        queue = deque([node])
 
-            # 如果節點已經訪問過，則返回已訪問的節點
-            # If the node has been visited, return the visited node
-            if node in visited:
-                return visited[node]
-            
-            # 如果節點還未訪問過，則複製該節點
-            # If the node has not been visited, copy the node
-            copy = Node(node.val)
-            # 將該節點標記為已訪問
-            # Mark the node as visited
-            visited[node] = copy
+        while queue:
+            # Get the next original node from the queue.
+            original_node = queue.popleft()
 
-            # 先遍歷該節點的所有鄰居
-            # Traverse all the neighbors of the node
-            for neighbor in node.neighbors:
-                # 遞歸訪問該節點的所有鄰居
-                # Recursively visit all the neighbors of the node
-                copy.neighbors.append(dfs(neighbor))
+            # Iterate through all its neighbors.
+            for neighbor in original_node.neighbors:
+                # If a neighbor has not been cloned yet...
+                if neighbor not in cloned_nodes:
+                    # Clone it and add it to the map.
+                    cloned_nodes[neighbor] = Node(neighbor.val)
+                    # and add the original neighbor to the queue to be processed later.
+                    queue.append(neighbor)
+                # Add the cloned neighbor to the neighbors list of the cloned current node.
+                cloned_nodes[original_node].neighbors.append(cloned_nodes[neighbor])
+        # Return the clone of the starting node.
+        return cloned_nodes[node]
 
-            return copy
-
-        return dfs(node)
-        
 # @lc code=end
 
