e0244a57f3905bf3b76d884d2c6d1db8bce42a45
Leif_Chang 張力夫 (MIS) <Leif_Chang@coolermaster.com.tw>
2025-07-25 16:52:24 +0800

    Completed in No.1115

--- 1115.print-foo-bar-alternately.py
+++ 1115.print-foo-bar-alternately.py
@@ -5,40 +5,69 @@
 #
 
 # @lc code=start
-from threading import Lock
-class FooBar:
-    # Synchronization
-    def __init__(self, n):
-        self.n = n
-        self.lock = Lock()
-        self.foo_turn = True
-    def foo(self, printFoo: 'Callable[[], None]') -> None:
-        
-        for i in range(self.n):
-            while True:
-                self.lock.acquire()
-                if self.foo_turn:
-                    # printFoo() outputs "foo". Do not change or remove this line.
-                    printFoo()
-                    self.foo_turn =False
-                    self.lock.release()
-                    break
-                else:
-                     self.lock.release()
-                    # time.sleep(0.0001) # Optionally yield CPU
+from threading import Semaphore, Lock, Condition
 
-    def bar(self, printBar: 'Callable[[], None]') -> None:
+class FooBar:
+	# 2. Semaphore
+    # def __init__(self, n):
+    #     self.n = n
+    #     # foo_sem is initialized to 1, allowing the first foo() to run.
+    #     self.foo_sem = Semaphore(1)
+    #     # bar_sem is initialized to 0, forcing bar() to wait initially.
+    #     self.bar_sem = Semaphore(0)
+
+
+    # def foo(self, printFoo: 'Callable[[], None]') -> None:
         
-        for i in range(self.n):
-            self.lock.acquire()
-            if not self.foo_turn:
-                # printBar() outputs "bar". Do not change or remove this line.
-                printBar()
-                self.foo_turn = True
-                self.lock.release()
-                break
-            else:
-                self.lock.release()
-                # time.sleep(0.0001)
+    #    for i in range(self.n):
+    #         # Wait for and acquire the permit for foo.
+    #         self.foo_sem.acquire()
+    #         # Execute the critical section.
+    #         printFoo()
+    #         # Release the permit for bar, allowing it to run.
+    #         self.bar_sem.release()
+
+
+    # def bar(self, printBar: 'Callable[[], None]') -> None:
+        
+    #      for i in range(self.n):
+    #         # Wait for and acquire the permit for bar. This will block until foo() releases it.
+    #         self.bar_sem.acquire()
+    #         # Execute the critical section.
+    #         printBar()
+    #         # Release the permit for foo, allowing the next iteration of foo() to run.
+    #         self.foo_sem.release()
+	
+    # 3. Condition Variable
+	def __init__(self, n):
+		self.n = n
+		self.lock = Lock()
+		self. condition = Condition(self.lock)
+		# A shared state variable indicating whose turn it is.
+		self.turn = 'foo'
+	def foo(self, printFoo: 'Callable[[], None]') -> None:
+		for i in range(self.n):
+			# `with self.lock` automatically acquires and releases the lock.
+			with self.lock:
+				# wait_for() is a convenient method that combines the while loop and wait().
+                # It waits until the predicate (the lambda function) returns True.
+				self.condition.wait_for(lambda: self.turn =='foo')
+				printFoo()
+				# Change the state to signal the other thread.
+				self.turn = 'bar'
+				# Wake up the other thread (bar) which is waiting on the condition.
+				self.condition.notify()
+	def bar(self, printBar: 'Callable[[], None]') -> None:
+		for i in range(self.n):
+			# `with self.lock` automatically acquires and releases the lock.
+			with self.lock:
+				# Wait until the predicate `self.turn == 'bar'` is true.
+				self.condition.wait_for(lambda: self.turn =='bar')
+				printBar()
+				# Change the state back to 'foo'.
+				self.turn = 'foo'
+				# Wake up the other thread (bar) which is waiting on the condition.
+				self.condition.notify()
+		
 # @lc code=end
 
---
+++ 1117.building-h-2-o.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=1117 lang=python3
+#
+# [1117] Building H2O
+#
+
+# @lc code=start
+class H2O:
+    def __init__(self):
+        pass
+
+
+    def hydrogen(self, releaseHydrogen: 'Callable[[], None]') -> None:
+        
+        # releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen()
+
+
+    def oxygen(self, releaseOxygen: 'Callable[[], None]') -> None:
+        
+        # releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen()
+# @lc code=end
+
